9e02dbc71cb6137e56278b661acf6836977e1c5b
Sujal <sujalkesharwani220@gmail.com>
2024-04-02 22:36:58 +0530

    Herobody 1st section done

--- src/components/herobody.tsx
+++ src/components/herobody.tsx
@@ -3,7 +3,7 @@ import halfCircleAtNavbar from "../images/halfCircleAtNavbar.png";
 import Marcus from "../images/Marcus Aurelius.png";
 import secondScientist from "../images/secondScienticist.png";
 import greenQuestionMark from "../images/green question mark.png";
-// import circle from "../images/circle.svg";
+import circle from "../images/circle.svg";
 
 const Herobody = () => {
   return (
@@ -14,27 +14,31 @@ const Herobody = () => {
           <img
             src={halfCircleAtNavbar}
             alt="halfCircleAtNavbar"
-            className="h-[110px] w-[98px] mb:h-[100] mb:w-[92px]"
+            className="h-[110px] w-[98px] mb:w-[92px]"
           />
-          <h1 className="mb:text-[40px] sm:text-[42px] md:text-[44px] lg:text-[48px] xl:text-[52px] 2xl:text-[56px] font-bold leading-snug md:leading-normal lgToLarge:leading-[1.6] mb:px-[20px] sm:px-[26px] md:px-[30px] lg:px-[34px] xl:px-[38px] 2xl:px-[42px] mb:w-[90vw] sm:w-[65vw] mb:pt-[60px] sm:pt-[60px]">
+
+          <h1 className="mb:text-[40px] sm:text-[42px] md:text-[44px] lg:text-[48px] xl:text-[52px] 2xl:text-[56px] font-bold leading-[1.5] md:leading-normal lgToLarge:leading-[1.6] mb:px-[20px] sm:px-[26px] md:px-[30px] lg:px-[34px] xl:px-[38px] 2xl:px-[42px] mb:w-[90vw] sm:w-[70vw] mb:pt-[60px] sm:pt-[60px]">
             ASK{" "}
-            <span className="text-yellow-300">
-              WHY&apos;S
+            <span className="text-yellow-500">
+              WHY&apos;s
               <span>
                 <img
                   src={orangeQuestionMark}
                   alt="orangeQuestionMark"
-                  className="w-[125px] h-[158px] relative top-[-120px] left-[340px]"
+                  className="w-[120px] h-[150px] mb:scale-[0.7] sm:scale-[0.75] md:scale-[0.8] lg:scale-[0.85] xl:scale-[0.9] inline mb:-ml-[30px] mb:-mt-[20px] sm:-ml-[20px] sm:-mt-[30px] mdToLarge:-mt-[30px] md:-ml-[20px] 2xl:-mt-[40px]"
                 />
               </span>
             </span>{" "}
-            <p className="relative top-[-150px] z-10">
-              AND LET THE ANSWERS GUIDE YOU TO YOUR JOURNEY OF CURIOUSITY
-              {/* <img
+            <p className="mb:-mt-[40px] sm:-mt-[37px] md:-mt-[34px] lg:-mt-[28px] xl:-mt-[26px] 2xl:-mt-[20px]">
+              <div className="relative z-10">
+                AND LET THE ANSWERS GUIDE YOU TO YOUR JOURNEY OF CURIOUSITY
+              </div>
+
+              <img
                 src={circle}
                 alt="circle"
-                className="w-[170px] h-[170px] relative top-[-100px] left-[10px] -z-10"
-              /> */}
+                className="w-[170px] h-[170px] mb:-mt-[80px] sm:-mt-[80px] md:-mt-[85px] lg:-mt-[90px] xl:-mt-[100px] 2xl:-mt-[105px]"
+              />
             </p>
           </h1>
         </div>
